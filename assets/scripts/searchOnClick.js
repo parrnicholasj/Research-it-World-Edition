50562b3b09721905bbe1a4e5d2d6655c0dc06ebc
JC <james.calingo@gmail.com>
2019-04-12 11:34:59 -0400

    working on HTML now

--- assets/scripts/searchOnClick.js
+++ assets/scripts/searchOnClick.js
@@ -1,31 +1,36 @@
-var country = ""
-
-
+var countryName = ""
 
 $("#search").on("click", function(event){
   event.preventDefault();
 
-  country = $("#country-input").val().trim();
+  countryName = $("#country-input").val().trim();
+  console.log(countryName)
 
-  if(country = ""){
-  $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
-    modal.find('.modal-body input').val(recipient)
-  })
-}
-
-
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })(); 
   
-  console.log(country);
+
   // Country info
-  $("#country-name").html(country);
-  $(".capital").html("what");
+  $("#country-name").html(countryName);
+  $(".capital").html("The Capital...");
   $("#language").html("most common language (i.e. what about Canada?)");
   $("#currency").html("MONEY MONEY MONEY MONEY");
+  $("#holidays").html("Upcoming state/federal holidays")
 
 // Weather data display
   $("#cloud-status").html("weatherresponse.cloudy")
@@ -41,20 +46,11 @@ $("#search").on("click", function(event){
     $articleList.addClass("list-group");
 
     // Add the newly created element to the DOM
-    $("#article-section").append($articleList);
+    $("#headlines").append($articleList);
 
     // If the article has a headline, log and append to $articleList
-    var headline = article.headline;
-    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
-
-    if (headline && headline.main) {
-      console.log(headline.main);
-      $articleListItem.append(
-        "<span class='label label-primary'>" +
-          articleCount +
-          "</span>" +
-          "<strong> " +
-          headline.main +
-          "</strong>")
-      }
+    var news = $("<div>");
+    news.addClass("card");
+    news.html("the articles");
+    $("#headlines").append(news)
     })
